0298165c021b7da2a3d1305f0d032330be43c88c
XilefTech <52332196+XilefTech@users.noreply.github.com>
2021-09-28 22:43:32 +0200

    added barebones login structure

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request
 import logging
 import sqlite3
 import webuntis
 
 app = Flask("UnitFilter")
 databaseName = 'database.db'
-database = 0
+database, cur = 0
 
 @app.route("/")
 def root():
     return redirect("/login")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return "login page"
+    error = None
 
-@app.route("/register")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        if email in cur.execute('SELECT e_mail FROM userdata'):
+            ## correct username
+            if password == cur.execute('SELECT password_has FROM userdata WHERE e_mail="{email}"'):
+                ## correct password/credentials
+                return redirect("/timetable")
+            else:
+                ## wrong password
+                error = 'Invalid Password. Please try again.'
+        else:
+            ## incorrect username
+            error = 'Invalid Username. Please try again.'
+
+    return render_template('login.html', error=error)
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     return "Register page"
 
@@ -29,7 +47,7 @@ def initDB():
 
     connection = sqlite3.connect(databaseName)
     createTableCode = '''CREATE TABLE IF NOT EXISTS userdata (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         e_mail          VARCHAR (255) UNIQUE,
         password_hash   VARCHAR (255),
         subjects        VARCHAR (255),
@@ -43,6 +61,8 @@ def initDB():
 
 
 if __name__ == '__main__':
+    global database, cur
     initDB()
     database = sqlite3.connect(databaseName)
+    cur = database.cursor()
     app.run(port=8888)
