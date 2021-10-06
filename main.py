d6b8b87ff0336da506fb0c702c281304ffb6167b
XilefTech <52332196+XilefTech@users.noreply.github.com>
2021-10-06 22:02:39 +0200

    added admin area

--- main.py
+++ main.py
@@ -134,6 +134,32 @@ def timetable():
 
     return "The Timetable"
 
+@app.route("/admin")
+def adminIndex():
+    return render_template('admin.html')
+
+@app.route("admin/addClass")
+def addClass():
+    if request.method == 'POST':
+        try:
+            database = sqlite3.connect(databaseName)
+            cur = database.cursor()
+
+            server = request.form['email']
+            school = request.form['password']
+            classname = request.form['repeat_password']
+            password = request.form['school']
+
+            cur.execute(f'''INSERT INTO schooldata (server, school, school, class, password)
+                        VALUES ("{server}", "{school}", "{classname}", "{password}");''')
+            database.commit()
+            cur.close()
+            
+            return render_template('addClass.html', message="Success")
+        except Exception as e:
+            return render_template('addClass.html', message=e)
+
+    return render_template('register.html', message=None)
 
 def initDB():
     logging.info("Initializing database...")
