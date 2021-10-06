5013ac07fd4f72977e11fea5522cc62b60c8f0dd
XilefTech <52332196+XilefTech@users.noreply.github.com>
2021-10-06 23:39:17 +0200

    added course selection check to timetable site

--- main.py
+++ main.py
@@ -130,12 +130,17 @@ def register():
 def updateCourseSelection():
     return 'nothing here yet'
 
-
 @app.route("/timetable")
 def timetable():
     ## check if user is not already logged in and redirect to timetable if that is the case
     if 'username' not in session:
         return redirect("/login")
+    
+    ## check for course selection and redirect, if no courses are selected
+    cur = sqlite3.connect(databaseName).cursor()
+    cur.execute(f'SELECT class_code FROM userdata WHERE e_mail={session["username"]}')
+    if cur.fetchall()[0][0] == None:
+        return redirect("/updateCourseSelection")
 
     return "The Timetable"
 
