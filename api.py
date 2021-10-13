b938b67506cba69ae8cd03ae9f96a2e0718cc08a
XilefTech <52332196+XilefTech@users.noreply.github.com>
2021-10-13 23:08:37 +0200

    added getAvailableSubjects function

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, render_template, request, session
 import sqlite3
 import config
+import webuntis
 
 databaseName = config.databaseName
 
@@ -14,8 +15,6 @@ def getAllClasses():
     cur.execute("SELECT * FROM schooldata")
     return str(cur.fetchall())
 
-
-
 @api.route("/api/getAllUsers")
 def getAllUsers():
     database = sqlite3.connect(databaseName)
@@ -25,3 +24,42 @@ def getAllUsers():
     userdata = cur.fetchall()
 
     return str(userdata)
+
+@api.route('/api/getAvailableSubjects')
+def getAvailableSubjects():
+    ## check if user is logged in
+    if 'username' not in session:
+        return
+
+    database = sqlite3.connect(databaseName)
+    cur = database.cursor()
+
+    cur.execute(f"SELECT school,class_code FROM userdata WHERE e_mail='{session['username']}'")
+    data = cur.fetchall()
+    school = data[0][0]
+    classname = data[0][1]
+
+    cur.execute(f"SELECT server,password FROM schooldata WHERE school='{school}' AND class='{classname}'")
+    data = cur.fetchall()
+    server = data[0][0]
+    password = data[0][1]
+
+    subjects = []
+
+    untisSession = webuntis.Session(
+        server=f'{server}.webuntis.com',
+        school=school,
+        username=classname,
+        password=password,
+        useragent=config.untisAgentName
+    )
+
+    with untisSession.login():
+        klasse=untisSession.klassen().filter(name=classname)[0]
+        timetable = untisSession.timetable(klasse=klasse, start=20211008, end=20211016)
+
+        for k in timetable:
+            if len(k.subjects):
+                subjects.append(k.subjects[0].name)
+
+    return str(set(subjects))
