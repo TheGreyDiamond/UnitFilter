c9a97d3a0ec0f199df916eba2ee6863a1fed77dc
XilefTech <52332196+XilefTech@users.noreply.github.com>
2021-10-15 23:57:39 +0200

    added getTimegridUnits

--- api.py
+++ api.py
@@ -67,8 +67,53 @@ def getAvailableSubjects():
 
     return str(set(subjects))
 
+@api.route("/api/getTimegridUnits")
+def getTimegridUnits():
+    ## check if user is logged in
+    if 'username' not in session:
+        return
+
+    database = sqlite3.connect(databaseName)
+    cur = database.cursor()
+
+    ## check for course selection and redirect, if no courses are selected
+    cur.execute(f'SELECT subjects FROM userdata WHERE e_mail={session["username"]}')
+    userSubjects = cur.fetchall()[0][0]
+    if userSubjects != None:
+        userSubjects = userSubjects.split(",")
+
+
+    ## gain school and class code for given user
+    cur.execute(f"SELECT school,class_code FROM userdata WHERE e_mail='{session['username']}'")
+    data = cur.fetchall()
+    school = data[0][0]
+    classname = data[0][1]
+
+    ## get server and password of the given school
+    cur.execute(f"SELECT server,password FROM schooldata WHERE school='{school}' AND class='{classname}'")
+    data = cur.fetchall()
+    server = data[0][0]
+    password = data[0][1]
+
+    untisSession = webuntis.Session(
+        server=f'{server}.webuntis.com',
+        school=school,
+        username=classname,
+        password=password,
+        useragent=config.untisAgentName
+    )
+
+    with untisSession.login():
+        timegrid = untisSession.timegrid_units()
+
+    return str(timegrid)
+
 @api.route("/api/getFilteredTimetable")
 def getFilteredTimetable(): 
+    ## check if user is logged in
+    if 'username' not in session:
+        return
+
     ## init database
     database = sqlite3.connect(databaseName)
     cur = database.cursor()
