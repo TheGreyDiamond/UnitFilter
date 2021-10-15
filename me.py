e7c994808c4bea18269b0ca2b98d64d9eb87051f
XilefTech <52332196+XilefTech@users.noreply.github.com>
2021-10-15 22:13:57 +0200

    added timetable filtering

--- me.py
+++ me.py
@@ -14,8 +14,8 @@ def updateCourseSelection():
     ## check if user is not already logged in and redirect to timetable if that is the case
     if 'username' not in session:
         return redirect("/login")
-        
-    return 'nothing here yet'
+
+    return render_template("updateCourseSelection.html")
 
 @userpages.route("/me/timetable")
 def timetable():
@@ -23,10 +23,48 @@ def timetable():
     if 'username' not in session:
         return redirect("/login")
     
-    ## check for course selection and redirect, if no courses are selected
-    cur = sqlite3.connect(databaseName).cursor()
-    cur.execute(f'SELECT class_code FROM userdata WHERE e_mail={session["username"]}')
-    if cur.fetchall()[0][0] == None:
-        return redirect("/updateCourseSelection")
+    ## init database
+    database = sqlite3.connect(databaseName)
+    cur = database.cursor()
 
-    return "The Timetable"
+    ## check for course selection and redirect, if no courses are selected
+    cur.execute(f'SELECT subjects FROM userdata WHERE e_mail={session["username"]}')
+    userSubjects = cur.fetchall()[0][0]
+    if userSubjects == None:
+        return redirect("/me/updateCourseSelection")
+    else:
+        userSubjects = userSubjects.split(",")
+
+
+    ## gain school and class code for given user
+    cur.execute(f"SELECT school,class_code FROM userdata WHERE e_mail='{session['username']}'")
+    data = cur.fetchall()
+    school = data[0][0]
+    classname = data[0][1]
+
+    ## get server and password of the given school
+    cur.execute(f"SELECT server,password FROM schooldata WHERE school='{school}' AND class='{classname}'")
+    data = cur.fetchall()
+    server = data[0][0]
+    password = data[0][1]
+
+    untisSession = webuntis.Session(
+        server=f'{server}.webuntis.com',
+        school=school,
+        username=classname,
+        password=password,
+        useragent=config.untisAgentName
+    )
+
+    filteredTimetable = []
+
+    with untisSession.login():
+        klasse=untisSession.klassen().filter(name=classname)[0]
+        timetable = untisSession.timetable(klasse=klasse, start=20211008, end=20211016)
+
+        for lesson in timetable:
+            if len(lesson.subjects):
+                if lesson.subjects[0].name in userSubjects:
+                    filteredTimetable.append(lesson)
+
+    return render_template("timetable.html", timetable=filteredTimetable)
